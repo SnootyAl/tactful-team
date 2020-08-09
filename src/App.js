7a46e7d85b015be3d83cc9733f58aa132d80d1e8
Calum Wilson <n10217487@qut.edu.au>
2020-08-09 15:43:03 +1000

    Updates to App.js
    Features: 
    Converts CSV file to JSON and saves it 
    Tagging: assigns a team number to a person and adds it to the JSON array

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-// test to see if branch is working
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// to install CSV to JSON: npm install csvtojson --save
+const CSVtoJSON = require("csvtojson")
+const FileSystem = require("fs")
+
+// create an array for people that have been assigned a team number 
+let teamarray = []
+
+// take csv file and convert it to JSON
+function csvtojson(){
+    CSVtoJSON().fromFile("./Big-5-Data.csv").then(source => {
+    FileSystem.writeFileSync("./Big-5-data.json", JSON.stringify(source));
+})}
+
+// import json file and tag persons with a team ID
+function createTeam(){
+    var personDATA = require("./Big-5-data.json")
+    // outer for loop controls the teams, inner for loop controls the amount of people assigned to a team
+    for(let i = 0; i < 2; i++){
+        for(let j = 0; j < 4; j++){
+            // tagging system: will take a person from the JSON file push it to the array, insert JSON key and Value, and remove itself 
+            teamarray.push(Object.assign(personDATA.shift(), {team: i}))
+        }
+    }
 }
 
-export default App;
+// for testing purposes
+function Teamloop(){
+    for(let i = 0; i < 8; i++){
+        console.log(teamarray.shift())
+    }
+}
+
+function start(){
+    // if Big-five-data.json is not in the folder comment out the last 2 functions and run, this will create the json file
+    csvtojson();
+    createTeam();
+    Teamloop(); 
+}
+
+start();
+
