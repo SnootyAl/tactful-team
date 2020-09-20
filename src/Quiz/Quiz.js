ffe6c4dff79680e85f1ebbc0e6a93a942fec72a6
Jonathan Raymond <jraym25@gmail.com>
2020-09-20 12:25:45 +1000

    Test now returns hash. Hash can be reverted into results

--- src/Quiz/Quiz.js
+++ src/Quiz/Quiz.js
@@ -2,14 +2,29 @@ import React, { useState, useEffect } from "react";
 import "./Quiz.css";
 import AnswerButton from "./AnswerButtons";
 import Question from "./Question";
+import cryptoJS from "crypto-js";
 
-const Quiz = (props) => {
-	const [questionNumberState, setQuestionNumberState] = useState(0);
+class Quiz extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			questionNumberState: 0,
+			answer: "",
+			questionsRemain: true,
+			questions: props,
+			name: "",
+			hashPlain: "",
+			hashString: "",
+			stage: 0,
+		};
+	}
 
-	const buttonPressHandler = (data) => {
-		//Do something with response also
+	buttonPressHandler = (data) => {
 		let response = data.target.value;
-		let currentQuestion = props.questions[questionNumberState];
+		console.log(this.state.questionNumberState + 1);
+		let currentQuestion = this.state.questions.questions[
+			this.state.questionNumberState
+		];
 		console.log(
 			currentQuestion.text,
 			"\nValue is: ",
@@ -19,23 +34,106 @@ const Quiz = (props) => {
 			"\nValue is: ",
 			currentQuestion.choices[response].score
 		);
+		const localQuestionsRemain = 119 != this.state.questionNumberState;
+		this.setState(
+			{
+				answer: this.state.answer + currentQuestion.choices[response].score,
+				questionNumberState: this.state.questionNumberState + 1,
+				questionsRemain: localQuestionsRemain,
+				stage: localQuestionsRemain ? 0 : 1,
+			},
+			() => {
+				console.log("answer");
+			}
+		);
+
 		// This is super ugly but also it works soooo
-		setQuestionNumberState(questionNumberState + 1);
+		//setQuestionNumberState(questionNumberState + 1);
 		// Now with less ugly!
 	};
 
-	return (
-		<div className="Quiz">
-			<Question
-				className="question"
-				question={props.questions[questionNumberState].text}
-			/>
-			<AnswerButton
-				className="answerButton"
-				click={buttonPressHandler.bind()}
-			/>
-		</div>
-	);
-};
+	renderQuizContent() {
+		return (
+			<div className="QuizQuestions">
+				<Question
+					className="question"
+					question={
+						this.state.questions.questions[this.state.questionNumberState].text
+					}
+				/>
+				<AnswerButton
+					className="answerButton"
+					click={this.buttonPressHandler.bind()}
+				/>
+			</div>
+		);
+	}
+
+	handleNameChange = (event) => {
+		this.setState({ name: event.target.value });
+	};
+
+	handleNameSubmit = (event) => {
+		const answer = this.state.answer;
+		const name = this.state.name;
+		const tempString = `CheckSum{-data-}${name}{-data-}${answer}{-data-}`;
+		const tempHash = cryptoJS.AES.encrypt(tempString, "Super Secret Key");
+		this.setState({
+			hashPlain: tempString,
+			hashString: tempHash.toString(),
+			stage: 2,
+		});
+	};
+
+	renderPostQuizContent = () => {
+		let content = (
+			<div className="QuizResults">
+				<form className="quizNameForm" onSubmit={this.handleNameSubmit}>
+					<input
+						className="quizNameInput"
+						type="text"
+						placeholder="Insert name..."
+						id="userInput"
+						autoComplete="off"
+						value={this.state.name}
+						onChange={this.handleNameChange}
+					/>
+					<input type="submit" value="Submit" />
+				</form>
+			</div>
+		);
+
+		return content;
+	};
+
+	renderResultContent = () => {
+		console.log(this.state.hashPlain);
+		let content = (
+			<div className="quizResult">
+				<p>{this.state.hashString}</p>
+			</div>
+		);
+		return content;
+	};
+
+	render() {
+		//console.log(this.state.questions.questions[0]);
+		let content;
+		switch (this.state.stage) {
+			case 0:
+				content = this.renderQuizContent();
+				break;
+			case 1:
+				content = this.renderPostQuizContent();
+				break;
+			case 2:
+				content = this.renderResultContent();
+		}
+		return <div className="Quiz">{content}</div>;
+	}
+	// const [questionNumberState, setQuestionNumberState] = useState(0);
+	// const [answer, addToAnswer] = useState("");
+	// let questionsRemain = true;
+}
 
 export default Quiz;
--- src/components/IndividualData.js
+++ src/components/IndividualData.js
@@ -10,10 +10,16 @@ class IndividualData extends React.Component {
 		this.state = {
 			inputValue: "",
 			title: props.title,
+			hash:
+				"U2FsdGVkX1+b309BPTC734hZPct9oOA9WIMzfne6ccNGfNM4rOFwDG4v44y6NNvco+FcuI7xbqzeqfl8ddWPNgCnA1jEJFQKaUy78SH+5L+PJS5JJROqHCiY0j9ATTU29iTjm1R/P07X5FVNJdYFdGNapDhMLuyN1QdCmOSL+HIKJU0NXIoFUdW3oHLYmMi8WcDsGhh/hyzakt2XCEbe0ZtHBjNyKFA3PYlFZeGQ9GmswL+kdy/WRiPU6LMUaTbo",
+			hasData: false,
+			data: [],
+			userName: "",
 		};
 
 		// HASH VALUE TO TEST DATA:
-		// Hash: U2FsdGVkX1+b309BPTC734hZPct9oOA9WIMzfne6ccNGfNM4rOFwDG4v44y6NNvco+FcuI7xbqzeqfl8ddWPNgCnA1jEJFQKaUy78SH+5L+PJS5JJROqHCiY0j9ATTU29iTjm1R/P07X5FVNJdYFdGNapDhMLuyN1QdCmOSL+HIKJU0NXIoFUdW3oHLYmMi8WcDsGhh/hyzakt2XCEbe0ZtHBjNyKFA3PYlFZeGQ9GmswL+kdy/WRiPU6LMUaTbo
+		// Hash:
+		// U2FsdGVkX1+b309BPTC734hZPct9oOA9WIMzfne6ccNGfNM4rOFwDG4v44y6NNvco+FcuI7xbqzeqfl8ddWPNgCnA1jEJFQKaUy78SH+5L+PJS5JJROqHCiY0j9ATTU29iTjm1R/P07X5FVNJdYFdGNapDhMLuyN1QdCmOSL+HIKJU0NXIoFUdW3oHLYmMi8WcDsGhh/hyzakt2XCEbe0ZtHBjNyKFA3PYlFZeGQ9GmswL+kdy/WRiPU6LMUaTbo
 		// Plain: The result was CheckSum{-data-}Jonathan{-data-}123451234512345123451234512345123451234512345123451234512345123451234512345123451234512345123451234512345123451234512345{-data-}
 		// I generated this with a local program I wrote, basically an AES encryption with secret key "Super Secret Key".
 		// Uses CryptoJS (npm install crypto-js).
@@ -22,12 +28,14 @@ class IndividualData extends React.Component {
 		//alert(`Called with ${this.state.inputValue}`);
 		const decryptedBytes = AES.decrypt(value, "Super Secret Key");
 		const plaintext = decryptedBytes.toString(cryptoJS.enc.Utf8);
+		console.log(value);
 		this.checkValue(plaintext);
-		console.log(plaintext);
+		//console.log(plaintext);
 	}
 
 	checkValue(value) {
 		const errCheck = value.slice(0, 8);
+		console.log(errCheck);
 		const result = value.slice(8);
 		// Check hash hasn't been malformed
 		// Remove checksum
@@ -41,30 +49,83 @@ class IndividualData extends React.Component {
 			// Get 5 domain numbers
 			const arrResults = this.recSumData(rawResults);
 			console.log(arrResults);
+			this.setState({ hasData: true, data: arrResults, userName: name });
 		} else {
 			alert("False");
 		}
 	}
 
+	createJsonTemplate() {
+		let result = {
+			totalC: {
+				f1: 0,
+				f2: 0,
+				f3: 0,
+				f4: 0,
+				f5: 0,
+				f6: 0,
+				total: 0,
+				domain: "Compassion",
+			},
+			totalA: {
+				f1: 0,
+				f2: 0,
+				f3: 0,
+				f4: 0,
+				f5: 0,
+				f6: 0,
+				total: 0,
+				domain: "Agreeableness",
+			},
+			totalN: {
+				f1: 0,
+				f2: 0,
+				f3: 0,
+				f4: 0,
+				f5: 0,
+				f6: 0,
+				total: 0,
+				domain: "Neuroticism",
+			},
+			totalO: {
+				f1: 0,
+				f2: 0,
+				f3: 0,
+				f4: 0,
+				f5: 0,
+				f6: 0,
+				total: 0,
+				domain: "Openness To Experience",
+			},
+			totalE: {
+				f1: 0,
+				f2: 0,
+				f3: 0,
+				f4: 0,
+				f5: 0,
+				f6: 0,
+				total: 0,
+				domain: "Extraversion",
+			},
+		};
+		return result;
+	}
+
 	// Recursive function to move down data string and use each element to sum its respective domain
 	recSumData(data) {
-		let myJson = {
-			totalC: 0,
-			totalA: 0,
-			totalN: 0,
-			totalO: 0,
-			totalE: 0,
-		};
+		let myJson = this.createJsonTemplate();
 
 		for (let i = 0; i < data.length; i++) {
 			let currentScore = parseInt(data.slice(i, i + 1));
-			console.log(currentScore);
 			let currentQuestion = questionData[i];
 
 			let added =
 				currentQuestion.keyed == "plus" ? currentScore : 6 - currentScore;
-			myJson[`total${currentQuestion.domain}`] += added;
-			console.log(myJson[`total${currentQuestion.domain}`]);
+			myJson[`total${currentQuestion.domain}`][
+				`f${currentQuestion.facet}`
+			] += added;
+			myJson[`total${currentQuestion.domain}`].total += added;
+			//console.log(myJson[`total${currentQuestion.domain}`]);
 		}
 		return [
 			myJson.totalC,
@@ -74,18 +135,58 @@ class IndividualData extends React.Component {
 			myJson.totalE,
 		];
 	}
+
 	handleInput = (tempInputValue) => {
 		this.setState({ inputValue: tempInputValue }, () => {
 			this.unHash(this.state.inputValue);
-			//alert(`Hello, ${this.state.inputValue}`);
 		});
 	};
 
+	renderUserData() {
+		const userData = this.state.data;
+		const userName = this.state.userName;
+		let domainElements = [];
+		for (let i = 0; i < 5; i++) {
+			let cD = userData[i];
+			let elements = (
+				<div className="domain" key={cD.domain}>
+					<h1>Domain: {cD.domain}</h1>
+					<p>Total: {cD.total}</p>
+					<p>
+						Facet 1: {cD.f1} | Facet 2: {cD.f2} | Facet 3: {cD.f4} | Facet 4:{" "}
+						{cD.f4} | Facet 5: {cD.f5} | Facet 6: {cD.f6}
+					</p>
+				</div>
+			);
+			domainElements.push(elements);
+		}
+		return (
+			<div className="domainResults">
+				<h1>User: {this.state.userName}</h1>
+				{/*need 5 className domain*/}
+
+				{domainElements}
+				<div className="domainHeader"></div>
+				{/*need 6 className domainFacet*/}
+				<div className="domainFacet"></div>
+			</div>
+		);
+	}
+
 	render() {
+		const doesHaveData = this.state.hasData;
+		let content;
+		if (doesHaveData) {
+			content = this.renderUserData();
+		} else {
+			content = <p>No Content :(</p>;
+		}
+
 		return (
 			<div className="showHome">
 				<h1 className="Home">{this.title}</h1>
 				<NameForm onInputHash={this.handleInput} />
+				{content}
 			</div>
 		);
 	}
